977437b9a00b666b846cd00045f81a47f970faea
keykey7 <29210485+keykey7@users.noreply.github.com>
2024-12-07 16:27:32 +0100

    review

--- src/test/java/ch/bbw/m320/restintro/api/v1/PonyControllerTest.java
+++ src/test/java/ch/bbw/m320/restintro/api/v1/PonyControllerTest.java
@@ -1,10 +1,22 @@
 package ch.bbw.m320.restintro.api.v1;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import ch.bbw.m320.restintro.dto.PonyDto;
 import ch.bbw.m320.restintro.entity.PonyEntity;
 import ch.bbw.m320.restintro.repository.PonyRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +25,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PonyControllerTest {
+class PonyControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,11 +41,14 @@ public class PonyControllerTest {
     @BeforeEach
     public void setup() {
         PonyEntity pony1 = new PonyEntity("Thunder", 10, 1.35, 350, true, LocalDate.of(2014, 5, 12), "Äpfel");
+        // this test is all about REST calls.
+        // calling the DB directly is like cheating. and it is not clean because you expose internal behaviour.
+        // instead use the POST method to create new ponies and avoid any dependency on internals.
         ponyRepository.save(pony1);
     }
 
     @Test
-    public void testGetPonies() throws Exception {
+    void testGetPonies() throws Exception { // no public test methods, there is no need. see sonar.
         mockMvc.perform(get("/api/ponies"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -63,6 +71,8 @@ public class PonyControllerTest {
 
     @Test
     public void testGetPonyByName() throws Exception {
+        // this test is NOT idempotent: if run by itself it will be green, together with the others will be red.
+        // we want tests to not have side effects.
         mockMvc.perform(get("/api/ponies/name/Thunder"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
